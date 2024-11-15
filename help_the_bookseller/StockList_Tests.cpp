b44f0b073a38bf475c9182c0bb795dda0f26be88
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-14 19:35:56 -0800

    First empty string test case passed for help the bookseller kata

--- help_the_bookseller/StockList_Tests.cpp
+++ help_the_bookseller/StockList_Tests.cpp
@@ -1,8 +1,17 @@
 #include <gtest/gtest.h>
+#include <vector>
 #include <string>
 #include "StockList.cpp"
 
 TEST(StockList_Test, BasicTest)
 {
+    // Arrange
     StockList unit = StockList();
+    std::vector<std::string> art = {"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+    std::vector<std::string> cd = {""};
+
+    // Act
+    std::string result = unit.stockSummary(art, cd);
+    // Assert
+    ASSERT_EQ("", result);
 }
