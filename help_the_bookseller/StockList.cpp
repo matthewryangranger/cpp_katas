f40500a85b20de0ebcbb6de8cb954587f241489f
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-14 19:27:45 -0800

    Initial set up

---
+++ help_the_bookseller/StockList.cpp
@@ -0,0 +1,21 @@
+#include <map>
+class StockList
+{
+public:
+    static std::string stockSummary(std::vector<std::string>& books, std::vector<std::string>& categories){
+        std::string return_str;
+        if (books.size() == 0 || categories.size() == 0) {
+            return_str = "";
+        } else {
+
+            for (std::string s: categories){
+
+            }
+
+
+            // Create the string
+        }
+
+        return return_str;
+    }
+};
---
+++ help_the_bookseller/StockList_Tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "StockList.cpp"
+
+TEST(StockList_Test, BasicTest)
+{
+    StockList unit = StockList();
+}
