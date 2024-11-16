6ed71b627e255a4508f232309dc3f310a6e2da7f
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-16 15:22:24 -0800

    Set up for next kata

---
+++ highest_and_lowest/HighestLowest.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+
+class HighestLowest
+{
+    public:
+    std::string highAndLow(const std::string& numbers)
+    {
+
+    }
+
+};
---
+++ highest_and_lowest/HighestLowest_Tests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "HighestLowest.cpp"
+
+TEST(HighestLowest_Test, BasicTest)
+{
+    // Arrange
+
+    // Act
+
+    // Assert
+}
