0afb6e74dd4e2ea405dc4d4f59d83164067dd1b1
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-17 13:00:54 -0800

    All test cases pass

--- highest_and_lowest/HighestLowest.cpp
+++ highest_and_lowest/HighestLowest.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 
 
 class HighestLowest
@@ -6,7 +7,22 @@ class HighestLowest
     public:
     std::string highAndLow(const std::string& numbers)
     {
+        int32_t high, low;
+        std::istringstream iss(numbers);
+        int32_t number;
 
+        while (iss >> number)
+        {
+            if (number < low)
+            {
+                low = number;
+            }
+            if (number > high)
+            {
+                high = number;
+            }
+        }
+        return std::to_string(high) + " " + std::to_string(low);
     }
 
 };
--- highest_and_lowest/HighestLowest_Tests.cpp
+++ highest_and_lowest/HighestLowest_Tests.cpp
@@ -4,8 +4,64 @@
 TEST(HighestLowest_Test, BasicTest)
 {
     // Arrange
+    HighestLowest unit = HighestLowest();
 
     // Act
+    std::string parameter = "5 6";
+    std::string result = unit.highAndLow(parameter);
 
     // Assert
+    ASSERT_EQ("6 5", result);
+}
+
+TEST(HighestLowest_Test, AscendingOrder)
+{
+    // Arrange
+    HighestLowest unit = HighestLowest();
+
+    // Act
+    std::string parameter = "1 2 3 4 5";
+    std::string result = unit.highAndLow(parameter);
+
+    // Assert
+    ASSERT_EQ("5 1", result);
+}
+
+TEST(HighestLowest_Test, IncludingNegative)
+{
+    // Arrange
+    HighestLowest unit = HighestLowest();
+
+    // Act
+    std::string parameter = "1 2 -3 4 5";
+    std::string result = unit.highAndLow(parameter);
+
+    // Assert
+    ASSERT_EQ("5 -3", result);
+}
+
+TEST(HighestLowest_Test, OutOfOrderWithNegative)
+{
+    // Arrange
+    HighestLowest unit = HighestLowest();
+
+    // Act
+    std::string parameter = "1 9 3 4 -5";
+    std::string result = unit.highAndLow(parameter);
+
+    // Assert
+    ASSERT_EQ("9 -5", result);
+}
+
+TEST(HighestLowest_Test, LargeListWithDuplicatesAndNegatives)
+{
+    // Arrange
+    HighestLowest unit = HighestLowest();
+
+    // Act
+    std::string parameter = "8 3 -5 42 -1 0 0 -9 4 7 4 -4";
+    std::string result = unit.highAndLow(parameter);
+
+    // Assert
+    ASSERT_EQ("42 -9", result);
 }
