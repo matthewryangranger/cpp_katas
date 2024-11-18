e64cb017809cfaa4b1979332c87b1d3d6427185b
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-18 12:58:38 -0800

    New Kata set up

---
+++ sum_between_numbers/Sum_Between_Numbers.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+class Sum_Between_Numbers
+{
+    int get_sum(int a, int b)
+    {
+        return 0;
+    }
+};
---
+++ sum_between_numbers/Sum_Between_Numbers_Tests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "Sum_Between_Numbers.cpp"
+
+TEST(Sum_Between_Numbers_Test, BasicTest)
+{
+    // Arrange
+
+    // Act
+
+    // Assert
+}
