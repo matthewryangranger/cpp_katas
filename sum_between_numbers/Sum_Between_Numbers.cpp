e2158c30ea1b515ac8cbdf912f39728802758ab2
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-19 13:20:03 -0800

    Finished Kata

--- sum_between_numbers/Sum_Between_Numbers.cpp
+++ sum_between_numbers/Sum_Between_Numbers.cpp
@@ -1,10 +1,26 @@
-#include <string>
-#include <vector>
-
 class Sum_Between_Numbers
 {
+    public:
+    void determineMax(const int& a, const int& b,  int& max,  int& min)
+    {
+        if (a > b)
+        {
+            max = a;
+            min = b;
+            return;
+        }
+        max = b;
+        min = a;
+    }
     int get_sum(int a, int b)
     {
-        return 0;
+        int max, min;
+        if (a == b)
+            return a;
+        determineMax(a, b, max, min);
+        int sum = min;
+        for (int i = min+1; i <= max; i++)
+            sum += i;
+        return sum;
     }
 };
--- sum_between_numbers/Sum_Between_Numbers_Tests.cpp
+++ sum_between_numbers/Sum_Between_Numbers_Tests.cpp
@@ -4,8 +4,74 @@
 TEST(Sum_Between_Numbers_Test, BasicTest)
 {
     // Arrange
+    Sum_Between_Numbers unit = Sum_Between_Numbers();
 
     // Act
+    int a = 0;
+    int b = 1;
+    int expected = 1;
+    int result = unit.get_sum(a, b);
 
     // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Sum_Between_Numbers_Test, NegativeNumberToPositiveTest)
+{
+    // Arrange
+    Sum_Between_Numbers unit = Sum_Between_Numbers();
+
+    // Act
+    int a = -1;
+    int b = 2;
+    int expected = 2;
+    int result = unit.get_sum(a, b);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Sum_Between_Numbers_Test, SameNumberTest)
+{
+    // Arrange
+    Sum_Between_Numbers unit = Sum_Between_Numbers();
+
+    // Act
+    int a = 1;
+    int b = 1;
+    int expected = 1;
+    int result = unit.get_sum(a, b);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Sum_Between_Numbers_Test, BetweenPositiveNumberTest)
+{
+    // Arrange
+    Sum_Between_Numbers unit = Sum_Between_Numbers();
+
+    // Act
+    int a = 2;
+    int b = 3;
+    int expected = 5;
+    int result = unit.get_sum(a, b);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Sum_Between_Numbers_Test, NegativeWithZero)
+{
+    // Arrange
+    Sum_Between_Numbers unit = Sum_Between_Numbers();
+
+    // Act
+    int a = 0;
+    int b = -1;
+    int expected = -1;
+    int result = unit.get_sum(a, b);
+
+    // Assert
+    ASSERT_EQ(expected, result);
 }
