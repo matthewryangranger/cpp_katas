1dbeb62f1b67ecfb9cf426573d4665d682a78235
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-19 13:26:07 -0800

    New Kata set up

---
+++ stop_spinning_my_words/Stop_Spinning_My_Words.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Stop_Spinning_My_Words
+{
+    public:
+    std::string spinWords(const std::string &str)
+    {
+        return "pizza";
+    }
+};
---
+++ stop_spinning_my_words/Stop_Spinning_My_Words_Tests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "Stop_Spinning_My_Words.cpp"
+
+TEST(Sum_Between_Numbers_Test, BasicTest)
+{
+    // Arrange
+    Stop_Spinning_My_Words unit = Stop_Spinning_My_Words();
+
+    // Act
+
+    // Assert
+}
+
