05b63c4e9c50a1b953e81bd4f0d7b0ee66e59377
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-21 20:29:13 -0800

    New Kata set up

---
+++ who_likes_it/Who_Likes_It.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Who_Likes_It
+{
+    public:
+    std::string likes(const std::vector<std::string> &names)
+    {
+        return "";
+    }
+};
---
+++ who_likes_it/Who_Likes_It_Tests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "Who_Likes_It.cpp"
+
+TEST(Who_Likes_It_Tests, BasicTest)
+{
+    // Arrange
+    Who_Likes_It unit = Who_Likes_It();
+
+    // Act
+
+    // Assert
+}
