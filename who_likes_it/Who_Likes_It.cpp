3c7f5bd1644a8917bd4c68caf4247b7cc46e61e7
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-21 21:20:49 -0800

    All test cases pass

--- who_likes_it/Who_Likes_It.cpp
+++ who_likes_it/Who_Likes_It.cpp
@@ -7,11 +7,30 @@ class Who_Likes_It
     std::string likes(const std::vector<std::string> &names)
     {
         std::string result;
+        int number_of_names = names.size();
         if (names.size() == 0)
             result += "no one";
+        int name_count = 0;
         for (std::string name : names)
-            result += name;
-        result += " likes this";
+        {
+            name_count++;
+            if (name_count > 1)
+                if (number_of_names >= 4)
+                {
+                    result += name + " and " + std::to_string(number_of_names - 2) + " others";
+                    break;
+                } else
+                name_count < number_of_names ? result += name : result += " and " + name;
+            else
+                if (number_of_names == 1)
+                    result += name;
+                else
+                    number_of_names == 2 ? result += name : result += name + ", ";
+        }
+        if (number_of_names <= 1)
+            result += " likes this";
+        else
+            result += " like this";
         return result;
     }
 };
--- who_likes_it/Who_Likes_It_Tests.cpp
+++ who_likes_it/Who_Likes_It_Tests.cpp
@@ -30,3 +30,76 @@ TEST(Who_Likes_It_Tests, OneNameTest)
     // Assert
     ASSERT_EQ(expected, result);
 }
+
+TEST(Who_Likes_It_Tests, TwoNamesTest)
+{
+    // Arrange
+    Who_Likes_It unit = Who_Likes_It();
+
+    // Act
+    std::vector<std::string> names;
+    names.push_back("Peter");
+    names.push_back("Alex");
+    std::string expected = "Peter and Alex like this";
+    std::string result = unit.likes(names);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Who_Likes_It_Tests, ThreeNamesTest)
+{
+    // Arrange
+    Who_Likes_It unit = Who_Likes_It();
+
+    // Act
+    std::vector<std::string> names;
+    names.push_back("Max");
+    names.push_back("John");
+    names.push_back("Mark");
+    std::string expected = "Max, John and Mark like this";
+    std::string result = unit.likes(names);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Who_Likes_It_Tests, FourNamesTest)
+{
+    // Arrange
+    Who_Likes_It unit = Who_Likes_It();
+
+    // Act
+    std::vector<std::string> names;
+    names.push_back("Alex");
+    names.push_back("Jacob");
+    names.push_back("Mark");
+    names.push_back("Max");
+    std::string expected = "Alex, Jacob and 2 others like this";
+    std::string result = unit.likes(names);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+
+TEST(Who_Likes_It_Tests, SevenNamesTest)
+{
+    // Arrange
+    Who_Likes_It unit = Who_Likes_It();
+
+    // Act
+    std::vector<std::string> names;
+    names.push_back("Alex");
+    names.push_back("Jacob");
+    names.push_back("Mark");
+    names.push_back("Joe");
+    names.push_back("Robert");
+    names.push_back("Derick");
+    names.push_back("Lou");
+    std::string expected = "Alex, Jacob and 5 others like this";
+    std::string result = unit.likes(names);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
