e9e5e0784dbcda50b17c7db5fff73091a0036175
Matthew Ryan Granger <mattg1827@gmail.com>
2024-11-21 20:18:14 -0800

    Finished tests, code works and kata submitted

--- stop_spinning_my_words/Stop_Spinning_My_Words.cpp
+++ stop_spinning_my_words/Stop_Spinning_My_Words.cpp
@@ -1,10 +1,32 @@
-#include <iostream>
+#include <regex>
+#include <sstream>
 
 class Stop_Spinning_My_Words
 {
     public:
     std::string spinWords(const std::string &str)
     {
-        return "pizza";
+        std::istringstream iss(str);
+        std::string word;
+        std::string result;
+
+        int word_count = 0;
+
+        while (iss >> word)
+        {
+            if (word.length() >= 5)
+                word_count == 0 ? result += reverseWord(word) : result += " " + reverseWord(word);
+            else
+                word_count == 0 ? result += word : result += " " + word;
+            word_count++;
+        }
+        return result;
+    }
+    std::string reverseWord(const std::string& str)
+    {
+        std::string result;
+        for (int i = str.length() - 1; i >= 0; i--)
+            result += str[i];
+        return result;
     }
 };
--- stop_spinning_my_words/Stop_Spinning_My_Words_Tests.cpp
+++ stop_spinning_my_words/Stop_Spinning_My_Words_Tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "Stop_Spinning_My_Words.cpp"
 
 TEST(Sum_Between_Numbers_Test, BasicTest)
@@ -7,7 +8,25 @@ TEST(Sum_Between_Numbers_Test, BasicTest)
     Stop_Spinning_My_Words unit = Stop_Spinning_My_Words();
 
     // Act
+    std::string parameter = "Hey fellow warriors";
+    std::string expected = "Hey wollef sroirraw";
+    std::string result = unit.spinWords(parameter);
 
     // Assert
+    ASSERT_EQ(result, expected);
+}
+
+TEST(Sum_Between_Numbers_Test, NoReverseWords)
+{
+    // Arrange
+    Stop_Spinning_My_Words unit = Stop_Spinning_My_Words();
+
+    // Act
+    std::string parameter = "This is a test";
+    std::string expected = "This is a test";
+    std::string result = unit.spinWords(parameter);
+
+    // Assert
+    ASSERT_EQ(result, expected);
 }
 
