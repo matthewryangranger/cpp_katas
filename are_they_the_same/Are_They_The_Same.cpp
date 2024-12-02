378f14aa074b68ce3ec4d6036ff159f296828e09
Matthew Ryan Granger <mattg1827@gmail.com>
2024-12-01 23:14:18 -0800

    Finished tests

--- are_they_the_same/Are_They_The_Same.cpp
+++ are_they_the_same/Are_They_The_Same.cpp
@@ -1,9 +1,35 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
 #include <vector>
+#include <cmath>
+#include <map>
 
 class Are_They_The_Same {
-    public :
-        static bool comp(std::vector<int>&a, std::vector<int>&b) {
-        // your code
-        return true;
+public:
+    static std::map<int, int> b_value_Count;
+    static std::map<int, int> a_value_Count;
+
+    static int reduceNumber(const int number) {
+        return static_cast<int>(sqrt(number));
+    }
+
+    static bool comp(std::vector<int>& a, std::vector<int>& b) {
+        a_value_Count.clear();
+        b_value_Count.clear();
+
+        if (a.size() != b.size())
+            return false;
+
+        for (int a_number : a)
+            a_value_Count[a_number]++;
+
+        for (int b_number : b) {
+            int reduced = reduceNumber(b_number);
+            if (reduced * reduced != b_number || a_value_Count.find(reduced) == a_value_Count.end())
+                return false;
+            b_value_Count[reduced]++;
+        }
+        return a_value_Count == b_value_Count;
     }
 };
+#endif // SOLUTION_H
--- are_they_the_same/Are_They_The_Same_Tests.cpp
+++ are_they_the_same/Are_They_The_Same_Tests.cpp
@@ -1,13 +1,27 @@
 #include <gtest/gtest.h>
 
 #include "Are_They_The_Same.cpp"
+std::map<int, int> Are_They_The_Same::b_value_Count;
+std::map<int, int> Are_They_The_Same::a_value_Count;
 
 TEST(Are_They_The_Same_Tests, BasicTest)
 {
     // Act
     bool expected = true;
+    std::vector<int> a = {11};
+    std::vector<int> b = {121};
+    bool result = Are_They_The_Same::comp(a, b);
+
+    // Assert
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Are_They_The_Same_Tests, AnotherTest)
+{
+    // Act
+    bool expected = false;
     std::vector<int> a = {121, 144, 19, 161, 19, 144, 19, 11};
-    std::vector<int> b = {14641, 20736, 361, 25921, 361, 20736, 361, 121};
+    std::vector<int> b = {14641, 20736, 361, 25921, 361, 20736, 362, 121};
     bool result = Are_They_The_Same::comp(a, b);
 
     // Assert
