80bff967a1381177591e075c295368c48876a8cd
Matthew Ryan Granger <mattg1827@gmail.com>
2025-01-03 23:18:05 -0800

    Refactored with two green passing tests

--- matrix_creation/Matrix_Creation.cpp
+++ matrix_creation/Matrix_Creation.cpp
@@ -8,7 +8,7 @@ public:
     if (n >= 0)
     {
       std::vector<std::vector<int>> matrix = {};
-      for (int i = 0; i < n; i++)
+      for (int i = 1; i <= n; i++)
       {
         auto row = constructRow(n, i);
         matrix.push_back(row);
@@ -21,14 +21,19 @@ public:
   std::vector<int> constructRow(const int n_matrix_value, const int iteration_value)
   {
     std::vector<int> row = {};
-  if (n_matrix_value == 0)
+  if (n_matrix_value != 0)
   {
-    return std::vector<int>{};
-  }
-    if (n_matrix_value == 1)
+    for (int i = 1; i <= iteration_value; i++)
     {
-      row.push_back(1);
-      return row;
+      if (i != iteration_value)
+      {
+        row.push_back(0);
+      } else
+      {
+        row.push_back(1);
+      }
     }
+  }
+    return row;
   }
 };
