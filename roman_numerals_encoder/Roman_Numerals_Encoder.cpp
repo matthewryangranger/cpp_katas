d2d7f2428cbf65efd0b6d1b466e002d19a4cc2e8
Matthew Ryan Granger <mattg1827@gmail.com>
2025-01-07 23:53:37 -0800

    New Kata set up

---
+++ roman_numerals_encoder/Roman_Numerals_Encoder.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+class Roman_Numerals_Encoder
+{
+public:
+  std::string solution(int number){
+    // convert the number to a roman numeral
+    return std::string("");
+  }
+};
---
+++ roman_numerals_encoder/Roman_Numerals_Encoder_Tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "Roman_Numerals_Encoder.cpp"
+
+TEST(Roman_Numerals_Encoder_Tests, BasicTest)
+{
+  // Arrange
+
+  // Act
+
+  // Assert
+
+}
